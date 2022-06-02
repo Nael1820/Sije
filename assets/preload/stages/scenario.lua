dee35dc7c9dd04e4f0f312dc0cd12c15397b2251
Nael1820 <94629276+Nael1820@users.noreply.github.com>
2022-06-02 14:58:54 +0800

    Add files via upload

---
+++ assets/preload/stages/scenario.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	makeLuaSprite('back', 'cj/stage_backk', -920, 500);
+	setScrollFactor('back', 0.6, 0.6);
+	addLuaSprite('back', false);
+	makeLuaSprite('front', 'cj/stage_front', -920, 700);
+	addLuaSprite('front', false);
+end
